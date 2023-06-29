9eb63a51ff9bd0e5095197ae8213f58a199f912d
sorrus-dev <amr89.dev@gmail.com>
2023-06-29 16:02:08 -0500

    integrando en navbar con flowbite

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/icons/logo.svg";
 import { useSelector } from "react-redux";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const courses = useSelector((state) => state).shoppingCartReducer.cart;
+  console.log(courses);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -14,79 +15,201 @@ const NavBar = () => {
   const links = [{ to: "/courses", name: "Cursos" }];
 
   const activeStyle = "font-bold";
+  const normalStyle =
+    "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   return (
-    <header className="bg-primary-blue w-full h-auto flex items-center justify-between  px-4 py-1">
-      <div className="-mr-2  flex basis-1/3 md:hidden">
-        <button onClick={toggleMenu}>
-          <svg
-            className={`h-6 w-6 ${isOpen ? "hidden" : "block"}`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/home">
+          <img src={logo} alt="logo" className="w-24 h-auto basis-1/3" />
+        </Link>
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            data-collapse-toggle="navbar-search"
+            aria-controls="navbar-search"
+            aria-expanded="false"
+            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+          <div className="relative hidden md:block">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Search icon</span>
+            </div>
+            <input
+              type="text"
+              id="search-navbar"
+              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search..."
             />
-          </svg>
-          <svg
-            className={`h-6 w-6 ${isOpen ? "block" : "hidden"}`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
+          </div>
+          <button
+            data-collapse-toggle="navbar-search"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-search"
+            aria-expanded="false"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
+            <span className="sr-only">Open menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
 
-      <NavLink to="/home">
-        <img src={logo} alt="logo" className="w-24 h-auto basis-1/3" />
-      </NavLink>
-      <div>
-        <nav
-          className={`bg-primary-purple basis-1/3 fixed z-10 top-16 left-0 p-4 w-1/2  transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          } transition-transform duration-300 ease-in-out md:static md:w-auto md:p-0 md:translate-x-0`}
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-search"
         >
-          <ul className="flex flex-col md:flex-row">
+          <div className="relative mt-3 md:hidden">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+
+            <input
+              type="text"
+              id="search-navbar"
+              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Buscar curso..."
+            />
+          </div>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {links.map((el) => (
               <NavLink
                 key={el.name}
                 to={el.to}
-                className={({ isActive }) => (isActive ? activeStyle : "")}
+                className={({ isActive }) =>
+                  isActive ? activeStyle : normalStyle
+                }
                 onClick={toggleMenu}
               >
                 {el.name}
               </NavLink>
             ))}
           </ul>
-        </nav>
+        </div>
       </div>
-      <nav>
-        <ul className="flex">
-          <li>
-            <NavLink to="/login">Iniciar Sesion</NavLink>
-          </li>
-          <li>
-            <NavLink to="/account">👤</NavLink>
-          </li>
-          <li>
-            <NavLink to="/checkout">🛒 {courses.length}</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    </nav>
   );
 };
 
 export default NavBar;
+
+/*  <header className="bg-primary-blue w-full h-auto flex items-center justify-between  px-4 py-1">
+  <div className="-mr-2  flex basis-1/3 md:hidden">
+    <button onClick={toggleMenu}>
+      <svg
+        className={`h-6 w-6 ${isOpen ? "hidden" : "block"}`}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+      <svg
+        className={`h-6 w-6 ${isOpen ? "block" : "hidden"}`}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+  </div>
+  
+
+  <div>
+    <nav
+      className={`bg-primary-purple basis-1/3 fixed z-10 top-16 left-0 p-4 w-1/2  transform ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 ease-in-out md:static md:w-auto md:p-0 md:translate-x-0`}
+    >
+      <ul className="flex flex-col md:flex-row">
+        {links.map((el) => (
+          <NavLink
+            key={el.name}
+            to={el.to}
+            className={({ isActive }) => (isActive ? activeStyle : "")}
+            onClick={toggleMenu}
+          >
+            {el.name}
+          </NavLink>
+        ))}
+      </ul>
+    </nav>
+  </div>
+  <nav>
+    <ul className="flex">
+      <li>
+        <NavLink to="/login">Iniciar Sesion</NavLink>
+      </li>
+      <li>
+        <NavLink to="/account">👤</NavLink>
+      </li>
+      <li>
+        <NavLink to="/checkout">🛒 {courses.length}</NavLink>
+      </li>
+    </ul>
+  </nav>
+</header>  */
