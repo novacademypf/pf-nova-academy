93232a47a9a11286aeca4b9f7d8d8a00e82c4248
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2023-06-30 00:06:59 -0300

    fix(SingUp) Registro y validacion de existencia de usuario ok

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { SIGN_UP_SUCCESS, SIGN_UP_FAILURE  } from "../action-type/action-types";
+import { SIGN_UP_SUCCESS, 
+  SIGN_UP_FAILURE, 
+  CHECK_EMAIL_EXISTENCE,
+  CHECK_EMAIL_EXISTENCE_SUCCESS,
+  CHECK_EMAIL_EXISTENCE_FAILURE,  } from "../action-type/action-types";
 
 
 export const signUpSuccess = (user) => {
@@ -13,5 +17,28 @@ export const signUpSuccess = (user) => {
     return {
       type: SIGN_UP_FAILURE,
       payload: error,
+    };
+  };
+
+  export const checkEmailExistence = (email) => {
+    return async (dispatch) => {
+      dispatch({ type: CHECK_EMAIL_EXISTENCE });
+  
+      try {
+        // realizo la consulta al back para verificar la existencia del email
+        const response = await axios.post('http://localhost:3001/user/signup', {
+          email,
+        });
+  
+        // Si el email existe, se devuelve un mensaje de error
+        if (response.data.exists) {
+          dispatch({ type: CHECK_EMAIL_EXISTENCE_FAILURE, payload: 'El email ya está registrado' });
+          console.log('El email ya está registrado')
+        } else {
+          dispatch({ type: CHECK_EMAIL_EXISTENCE_SUCCESS });
+        }
+      } catch (error) {
+        dispatch({ type: CHECK_EMAIL_EXISTENCE_FAILURE, payload: 'Error al verificar el email' });
+      }
     };
   };
