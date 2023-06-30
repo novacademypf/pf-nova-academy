95fd075b09692b0f0051917c81ceb09f6f9232bd
sorrus-dev <amr89.dev@gmail.com>
2023-06-29 20:23:26 -0500

    arreglando el searchbar

--- client/src/redux/actions/allCategoriesActions.js
+++ client/src/redux/actions/allCategoriesActions.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 import { GET_ALL_CATEGORIES } from "../action-type/action-types";
 
-
-
-    export function getAllCategories() {
-        return async function (dispatch) {
-            let json = await axios.get('http://localhost:3001/category', {})
-            return dispatch({
-                type: GET_ALL_CATEGORIES,
-                payload: json.data
-            })
-        }
-    }
+export function getAllCategories() {
+  return async function (dispatch) {
+    let json = await axios.get("http://localhost:3001/category");
+    return dispatch({
+      type: GET_ALL_CATEGORIES,
+      payload: json.data,
+    });
+  };
+}
