1635453583e48e50f376f7d0b01fb83ffa44e7b5
danielpaz23 <danielpaz2233@gmail.com>
2023-07-01 21:28:53 -0300

    createCourse, en proceso, validaciones funcionando, falta conectar front con back para hacer el post de todo

--- client/src/pages/CreateCourse/ModuleCreate/index.jsx
+++ client/src/pages/CreateCourse/ModuleCreate/index.jsx
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import CreateLesson from "../LessonCreate";
+import axios from "axios";
 
 export default function FormCourse() {
+  const dispatch = useDispatch();
   const [lesson, setLesson] = useState(0);
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+  });
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+  });
   const renderLesson = () => {
     return Array.from({ length: lesson }, (_, index) => (
       <CreateLesson key={index} />
     ));
   };
 
-  const addLesson = () => {
+  const addLesson = async (event) => {    
+    event.preventDefault();
+    if(!form.name){
+      return alert("Ingrese Nombre");
+    } else if(!form.description){
+      return alert("Ingrese Descripcion");
+    } 
+    // await axios.post("http://localhost:3001/module/createModule", form, {
+    //   headers:{
+    //     Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFVzZXIiOjEsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg4MjUyMzU2LCJleHAiOjE2ODgyNTk1NTZ9.8XJ0SexydPrtqZ7YUVM76wLSd9Bgiyj9IAUggS75qKw",
+    //   },
+    // });
+    alert("Modulo creado, Agrega leccion")
     setLesson(lesson + 1);
   };
 
@@ -18,6 +41,27 @@ export default function FormCourse() {
     setLesson(lesson - 1);
   };
 
+  const changeHandler = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    const updatedForm = { ...form, [property]: value };
+    setForm(updatedForm);
+    setErrors(validate(updatedForm));
+  };
+  const validate = (form)=>{
+    let errores = {};
+    if(!form.name){
+      errores.name = "Ingrese Nombre";
+    } else {
+      errores.name = "";
+    }
+    if(!form.description){
+      errores.description = "Ingrese Descripcion";
+    } else {
+      errores.description = "";
+    }
+    return errores;
+  }
   return (
     <div className="mx-7">
       <h1 className="text-2xl pt-10">DATOS DEL MODULO</h1>
@@ -27,13 +71,24 @@ export default function FormCourse() {
           <input
             type="text"
             className="w-96 p-2 mb-4 border border-gray-300 rounded"
+            value={form.name}
+            onChange={changeHandler}
+            name="name"
           />
-
+          <div>
+            {errors.name && <span>{errors.name}</span>}
+          </div>
           <label className="block mb-2 font-bold">Descripcion:</label>
           <textarea
             className="w-full p-2 mb-4 border border-gray-300 rounded resize-none"
-            onChange={(e) => setDescription(e.target.value)}
+            value={form.description}
+            onChange={changeHandler}
+            name="description"
+            // onChange={(e) => setDescription(e.target.value)}
           ></textarea>
+          <div>
+            {errors.description && <span>{errors.description}</span>}
+          </div>
         </div>
       </div>
       <div className="flex justify-center">
--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -2,8 +2,9 @@ import { combineReducers } from "redux";
 import shoppingCartReducer from "./shoppingCartReducer.js";
 import coursesReducer from "./coursesReducer.js";
 import userReducer from "./userReducer.js";
-
+import categoriesReducer from "./categoryReducer.js"
 const reducer = combineReducers({
+  categoriesReducer: categoriesReducer,
   shoppingCartReducer: shoppingCartReducer,
   coursesReducer: coursesReducer,
   userReducer: userReducer
