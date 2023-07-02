71e5d1411002ed0ac0c9db3e4cedddc6a1a5ff6b
AliciaCardinali <aliciacardinali@hotmail.com>
2023-07-02 18:07:06 -0300

    carrousel con llamados a db

--- client/src/components/Carrousel/index.jsx
+++ client/src/components/Carrousel/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Carrousel = () => {
   const [images, setImages] = useState([]);
@@ -10,7 +11,7 @@ const Carrousel = () => {
     const imageArray = [];
     for (let i = 0; i < 100; i++) {
       const imageObject = {
-        src: `https://picsum.photos/800/600?random=${i}`,
+        src: `https://source.unsplash.com/random/800x600/?book=${i}`,
         alt: `Imagen ${i}`,
       };
       imageArray.push(imageObject);
@@ -41,14 +42,25 @@ const Carrousel = () => {
   return (
     <div className=" carousel relative mb-4 ">
       <div className="flex">
-        {images.slice(currentIndex, currentIndex + 5).map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            className="w-1/5 h-auto object-cover p-2 carousel-item"
-          />
-        ))}
+        {images.slice(currentIndex, currentIndex + 5).map((image, index) => {
+          const courseId = courses && courses[index] && courses[index].id;
+          return (
+            <div
+              key={index}
+              className="w-1/5 h-auto object-cover p-2 carousel-item"
+            >
+              <Link to={`/detail/${courseId}`}>
+                <img src={image.src} alt={image.alt} className="w-full" />
+              </Link>
+              {courses && courses[index] && (
+                <div className="text-center">
+                  <h3 className="text-lg font-bold">{courses[index].name}</h3>
+                  <p className="text-sm font-medium">${courses[index].price}</p>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
       <button
         className="absolute top-1/2 left-0 transform -translate-y-1/2 px-4 py-2  border-radius: 0.25rem hover:underline rounded-full"
