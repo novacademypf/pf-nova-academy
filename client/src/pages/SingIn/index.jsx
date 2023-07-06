422959f4e76b90da1be3f612d4cb435fc5f10e3b
santiagoweb212 <juanusca45@gmail.com>
2023-07-06 18:04:38 -0500

    delete comentarios

--- client/src/pages/SingIn/index.jsx
+++ client/src/pages/SingIn/index.jsx
@@ -1,15 +1,6 @@
-import jwt_decode from "jwt-decode";
-
 import { FormSingIn } from "../../components/formSingIn/Formulario";
-import { useGoogleAuth } from "../../hooks/useGoogleAuth";
-import { loginUserGoogle } from "../../services/loginUserRequest";
-import { useState } from "react";
 import GoogleLogin from "./GoogleLogin";
 
-/* eslint-disable */
-// import { useHistory } from 'react-router-dom';
-// https://accounts.google.com/gsi/select?client_id=50733054185-1gaasavkm9laqosq23fv4qpv3ov62fgu.apps.googleusercontent.com&ux_mode=popup&ui_mode=card&as=xHsHkLjUndEoqG7j2Z0Y0g&channel_id=bfc9a4035c547900d3f09ca5650ab51f549424ff1bec86741104b787b4357ea9&origin=http%3A%2F%2Flocalhost%3A5173
-/* https://accounts.google.com/gsi/select?client_id=50733054185-1gaasavkm9laqosq23fv4qpv3ov62fgu.apps.googleusercontent.com&ux_mode=popup&ui_mode=card&as=C%2B%2Fqu3qu4TMEYgmi14%2F20Q&channel_id=016f8665d743109228f3d9a9e5667e52506da28c40fc341974d89c42207ec0d0&origin=https%3A%2F%2F36gyt9.csb.app */
 const SingIn = () => {
   return (
     <section className="h-[calc(100vh-4.1em)] flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0  border-neutral-800">
