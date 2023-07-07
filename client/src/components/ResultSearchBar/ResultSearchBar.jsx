0362e1518e9c9fafa287dcbe55e83d9d1994a6f3
santiagoweb212 <juanusca45@gmail.com>
2023-07-06 23:25:33 -0500

    add operador and para envitar problemas de undefined

--- client/src/components/ResultSearchBar/ResultSearchBar.jsx
+++ client/src/components/ResultSearchBar/ResultSearchBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ResultSearchBar = ({ results }) => {
   return (
     <div className="flex flex-row flex-wrap max-w-screen-md">
-      {results.map((el) => (
+      {results && results.map((el) => (
         <Link
           className=" flex flex-row items-center bg-gray-50 border border-[#00FFFF] hover:bg-cyan-400 text-gray-900 text-lg font-bold rounded-lg focus:shadow-lg w-full pl-10 p-2.5 m-0.5"
           key={el.id}
