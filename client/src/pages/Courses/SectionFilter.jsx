d3a4e39678c7f5f879b2f1c2098c10aee1bd4e05
liliana Ruiz <liliana.ruiz@yahoo.com>
2023-07-07 18:26:47 -0500

    estilos filtros paginado

--- client/src/pages/Courses/SectionFilter.jsx
+++ client/src/pages/Courses/SectionFilter.jsx
@@ -97,10 +97,12 @@ const handleChangeRange =(e)=>{
 
   return (
   
-    <section className=" bg-green-200 w-[15em] min-w-[15em] py-[2em] px-[1em]  fixed  z-50 left-[0] h-[calc(100vh-5.5em)] right-0 flex-col  overflow-auto justify-center">
+    <section className=" bg-purple-300 w-[15em] min-w-[15em] py-[2em] px-[1em]  fixed  z-50 left-[0] h-[calc(100vh-5.5em)] right-0 flex-col  overflow-auto justify-center">
+      
+      <h2 className="mb-5">Filtra Por Categoria:</h2>
       <Dropdown
-        labelValue={"Filtrar Categorias:"}
-        isLabel={isLabel}
+        //labelValue={"Filtrar Categorias:"}
+        isLabel={"Filtrar Categorias"}
         isOpen={isOpen}
         selectedOption={filters.category}
         toggleDropdown={toggleDropdown}
@@ -116,10 +118,11 @@ const handleChangeRange =(e)=>{
       >
         borrar filtros
       </button>*/}
-
+      <div className="mb-8">                               </div>
+      <h2 className="mb-0">Filtra Por Orden Alfabetico:</h2>
       <div className="relative inline-block w-64" />
       <select
-        className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2.4 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-sm"
+        className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 p-3 py-3 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-base"
         name="orderAlphabetico"
         value={filters.orderAlphabetico}
         onChange={handleSortByName}
@@ -130,16 +133,15 @@ const handleChangeRange =(e)=>{
       </select>
 
       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg className="fill-current h-4 w-4" viewBox="0 0 20 20">
+        <svg className="fill-current h-4 w-4" viewBox="0 0 20 20 transform rotate-180">
           <path d="M10 12l-6-6 1.5-1.5L10 9l4.5-4.5L16 6l-6 6z" />
         </svg>
       </div>
-
+      <div className="mb-4"></div>
       <label
         for="default-range"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-       Seleccione el Rango de Precio
+      >Seleccione el Rango de Precio
       </label>
       <input
         id="default-range"
