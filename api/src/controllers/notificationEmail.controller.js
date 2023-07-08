3d511871bc3ab29ed075094376680c2ac04fae2d
Agustin <atunez30@gmail.com>
2023-07-08 20:34:19 -0300

    actualizada

---
+++ api/src/controllers/notificationEmail.controller.js
@@ -0,0 +1,15 @@
+const nodemailer = require("nodemailer");
+const { NODEMAILER_PASS, NODEMAILER_EMAIL } = process.env;
+console.log("entra a nodemailer")
+const transporter = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: NODEMAILER_EMAIL,
+    pass: NODEMAILER_PASS
+  }
+});
+transporter.verify().then(()=>{
+  console.log("Ready for send emails")
+})
