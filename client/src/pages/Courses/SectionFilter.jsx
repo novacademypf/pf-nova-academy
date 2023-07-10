cac131321c221bad14a4f410e3eed4cc56b0c9c6
liliana Ruiz <liliana.ruiz@yahoo.com>
2023-07-09 23:25:06 -0500

    corregido error por consola

--- client/src/pages/Courses/SectionFilter.jsx
+++ client/src/pages/Courses/SectionFilter.jsx
@@ -86,13 +86,7 @@ const SectionFilter = () => {
   }
 
   return (
-    <section className=" bg-purple-300 w-[15em] min-w-[15em] py-[2em] px-[1em]  fixed  z-50 left-[0] h-[calc(100vh-5.5em)] right-0 flex-col  overflow-auto justify-center">
-      <h2 className="mb-5">Filtra Por Categoria:</h2>
-      <Dropdown
-        //labelValue={"Filtrar Categorias:"}
-        isLabel={"Filtrar Categorias"}
-      />
-      <section className=" bg-purple-300 w-[15em] min-w-[15em] py-[2em] px-[1em]  fixed  z-50 left-[0] h-[calc(100vh-5.5em)] right-0 flex-col  overflow-auto justify-center"></section>
+      <section className=" bg-purple-300 w-[15em] min-w-[15em] py-[2em] px-[1em]  fixed  z-50 left-[0] h-[calc(100vh-5.5em)] right-0 flex-col  overflow-auto justify-center">
       <h2 className="mb-5">Filtra Por Categoria:</h2>
       <Dropdown
         islabel={"Filtrar Categorias"}
