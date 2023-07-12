aad93e744d3d95b5263c5b89c4f1efeff6fac8d2
liliana Ruiz <liliana.ruiz@yahoo.com>
2023-07-12 15:37:35 -0500

    cambios

--- client/src/pages/Courses/SectionCursos.jsx
+++ client/src/pages/Courses/SectionCursos.jsx
@@ -60,8 +60,10 @@ const SectionCursos = () => {
           renderOnZeroPageCount={null}
         />
       </div>
+      <div className="sticky top-[3.5em] overflow-y-auto h-[calc(100vh-5.5em)]">
       <div className="w-full relative top-[3.5em]">
         <CourseCards courses={paginatedCourses} />
+      </div>
       </div>
       
     </section>
