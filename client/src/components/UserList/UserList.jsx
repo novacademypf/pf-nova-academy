1d8cbbead1dbbbd4b0c429f8db05bd3e984dcaff
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2023-07-14 22:41:31 -0300

    Feat(UserList)Confirmacion de borrado de usuarios,alert congif

--- client/src/components/UserList/UserList.jsx
+++ client/src/components/UserList/UserList.jsx
@@ -3,9 +3,6 @@ import { useDispatch } from 'react-redux';
 import { deleteUser } from '../../redux/actions/userActions';
 import Swal from "sweetalert2";
 
-
-
-
 const UserList = ({ users }) => {
   const dispatch = useDispatch();
   const [deletedUserIds, setDeletedUserIds] = useState([]);
@@ -16,8 +13,6 @@ const UserList = ({ users }) => {
   const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
   useEffect(() => {
-    // Realiza cualquier acción necesaria después de eliminar un usuario
-    // Puede ser actualizar la lista de usuarios o cualquier otra acción
   }, [deletedUserIds]);
 
   const handleDeleteUser = (userId) => {
@@ -26,13 +21,27 @@ const UserList = ({ users }) => {
     setDeletedUserIds([...deletedUserIds, userId]);
   };
 
+  const showConfirmationAlert = (userId, userName) => {
+    Swal.fire({
+      title: 'Confirmación',
+      text: `¿Está seguro que desea eliminar al usuario ${userName}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteUserWithAlert(userId);
+      }
+    });
+  };
 
   const showAlert = () => {
     Swal.fire({
       title: 'Éxito',
       text: 'Usuario eliminado correctamente',
       icon: 'success',
-      confirmButtonText: 'Aceptar'
+      confirmButtonText: 'Aceptar',
     });
   };
 
@@ -41,7 +50,7 @@ const UserList = ({ users }) => {
       title: 'Error',
       text: error.message,
       icon: 'error',
-      confirmButtonText: 'Aceptar'
+      confirmButtonText: 'Aceptar',
     });
   };
 
@@ -56,96 +65,88 @@ const UserList = ({ users }) => {
 
   const totalPages = Math.ceil(users.length / usersPerPage);
 
-
   const handlePrevPage = () => {
-   if (currentPage > 1) {
-     setCurrentPage(currentPage - 1);
-   }
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   const handleNextPage = () => {
-   if (currentPage < totalPages) {
-     setCurrentPage(currentPage + 1);
-   }
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
   };
 
-  if(currentUsers === 0) {
-    return <h2>No existen usuarios registrados por el momento</h2>
+  if (currentUsers.length === 0) {
+    return <h2>No existen usuarios registrados por el momento</h2>;
   }
 
   return (
     <>
-            <div className="xl:w-3/4 2xl:w-4/5 w-full">
-                <div className="px-4 md:px-10 py-4 md:py-7">
-                    <div className="sm:flex items-center justify-between">
-                        <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Usuarios Activos {users.length}</p>
-                    </div>
-                </div>
-                {currentUsers.map((user) => {
-                  if (deletedUserIds.includes(user.userId)) {
-                    return null; // Omitir el renderizado del usuario eliminado
-                  }
-                  return (
-                <div className="bg-white px-4 md:px-10 pb-5" key={user.userId}>
-                    <div className="overflow-x-auto">
-                        <table className="w-full whitespace-nowrap">
-                            <tbody>
-                                <tr className="text-sm leading-none text-gray-600 h-16">
-                                    <td className="w-1/2">
-                                        <div className="flex items-center">
-                                            <div className="w-10 h-10 bg-gray-700 rounded-sm flex items-center justify-center">
-                                            <img
-                                                  className="hidden xl:block w-full "
-                                                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsEC-AcpMSEBeqwQdUVhjb5fciR-GG2-cuwQ&usqp=CAU"
-                                                  alt="avatar"
-                                             />
-                                            </div>
-                                            <div className="pl-2">
-                                                <h2 className="text-sm font-medium leading-none text-gray-800">{user.name}</h2>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className="pl-16">
-                                        <p>{user.email}</p>
-                                    </td>
+      <div className="xl:w-3/4 2xl:w-4/5 w-full">
+        <div className="px-4 md:px-10 py-4 md:py-7">
+          <div className="sm:flex items-center justify-between">
+            <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Usuarios Activos {users.length}</p>
+          </div>
+        </div>
+        {currentUsers.map((user) => {
+          if (deletedUserIds.includes(user.userId)) {
+            return null; // Omitir el renderizado del usuario eliminado
+          }
+          return (
+            <div className="bg-white px-4 md:px-10 pb-5" key={user.userId}>
+              <div className="overflow-x-auto">
+                <table className="w-full whitespace-nowrap">
+                  <tbody>
+                    <tr className="text-sm leading-none text-gray-600 h-16">
+                      <td className="w-1/2">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 bg-gray-700 rounded-sm flex items-center justify-center">
+                            <img
+                              className="hidden xl:block w-full"
+                              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsEC-AcpMSEBeqwQdUVhjb5fciR-GG2-cuwQ&usqp=CAU"
+                              alt="avatar"
+                            />
+                          </div>
+                          <div className="pl-2">
+                            <h2 className="text-sm font-medium leading-none text-gray-800">{user.name}</h2>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="pl-16">
+                        <p>{user.email}</p>
+                      </td>
 
-                                    <td>
-                                    <button 
-                                    className=" bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"              
-                                    onClick={() => deleteUserWithAlert(user.userId)}>Eliminar
-                                             
-                                    </button>
-                                    </td>
-                                </tr>
-                                
-                               
-                            </tbody>
-                            
-                        </table>
-                        
-                        
-                    </div>
-                    
-                    
-                </div>
-                
-                 )})}
-                 <div className="flex items-center justify-center">
-                  <button className=" bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"              
-
-                  onClick={handlePrevPage}>Anterior</button>
-                  <p className="mx-4">Página {currentPage} de {totalPages}</p>
-                  <button className=" bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"              
-
-                  onClick={handleNextPage}>Siguiente</button>
-                  </div>
-
+                      <td>
+                        <button
+                          className="bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                          onClick={() => showConfirmationAlert(user.userId, user.name)}
+                        >
+                          Eliminar
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-           
-        </>
-
-
-
+          )
+        })}
+        <div className="flex items-center justify-center">
+          <button className="bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            onClick={handlePrevPage}
+          >
+            Anterior
+          </button>
+          <p className="mx-4">Página {currentPage} de {totalPages}</p>
+          <button className="bg-[#00FFFF] hover:bg-cyan-200 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            onClick={handleNextPage}
+          >
+            Siguiente
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
