e70a460633870334ec817410539d4fd62ed79cf2
AliciaCardinali <aliciacardinali@hotmail.com>
2023-07-15 00:02:40 -0300

    agregando rating a las estrellitas

--- client/src/components/CourseCard/CourseCard.jsx
+++ client/src/components/CourseCard/CourseCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/actions/shoppingCartActions";
 import { Link } from "react-router-dom";
@@ -10,6 +11,40 @@ const CourseCard = ({ dataCard }) => {
   const handleCart = (data) => {
     dispatch(addToCart(data));
   };
+
+  // Definir un estado para la calificación de estrellas (inicialmente 0)
+  const [rating, setRating] = useState(0);
+
+  // Definir una función para manejar la calificación de estrellas
+  const handleRating = (value) => {
+    setRating(value);
+  };
+
+  // Crear un array para renderizar las estrellas vacías o llenas según la calificación
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(
+        <button
+          key={i}
+          className="text-xl mr-2 text-yellow-500"
+          onClick={() => handleRating(i)}
+        >
+          ★
+        </button>
+      );
+    } else {
+      stars.push(
+        <button
+          key={i}
+          className="text-xl mr-2"
+          onClick={() => handleRating(i)}
+        >
+          ☆
+        </button>
+      );
+    }
+  }
 
   return (
     <div className="relative w-full max-w-sm shadow bg-gray-400 rounded-md p-3">
@@ -31,7 +66,8 @@ const CourseCard = ({ dataCard }) => {
       <div className="flex flex-col justify-between mb-2">
         <span className="text-md font-bold">{name}</span>
         <span className="text-sm font-light">{description}</span>
-        <span>⭐⭐⭐⭐⭐</span>
+        {/* Mostrar las estrellas según la calificación */}
+        <div>{stars}</div>
       </div>
       <div>
         <span className="text-lg font-medium">${price}</span>
